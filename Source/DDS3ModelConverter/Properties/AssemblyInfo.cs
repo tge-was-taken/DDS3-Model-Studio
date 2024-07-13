e4ed9873ac3cbf330394726bc082c06d97bafb61
tge-was-taken <tge-was-taken@outlook.com>
2024-07-13 13:55:58 +0200

    Upgrade projects to new SDK style format

--- Source/DDS3ModelConverter/Properties/AssemblyInfo.cs
+++ Source/DDS3ModelConverter/Properties/AssemblyInfo.cs
@@ -3,18 +3,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle( "DDS3ModelConverter" )]
-[assembly: AssemblyDescription( "" )]
-[assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "DDS3ModelConverter" )]
-[assembly: AssemblyCopyright( "Copyright © TGE 2019" )]
-[assembly: AssemblyTrademark( "" )]
-[assembly: AssemblyCulture( "" )]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -22,19 +10,6 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid( "180cc7a2-1a4e-42ec-ad44-09cef7f93c97" )]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( AssemblyInfoConstants.Version )]
-[assembly: AssemblyFileVersion( AssemblyInfoConstants.Version )]
 
 internal class AssemblyInfoConstants
 {
